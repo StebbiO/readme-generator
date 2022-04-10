69811e52f68cba303c445caf5ebbb34502f6ea06
Stebbi <stefodd@yahoo.com>
2022-04-10 14:48:12 -0500

    write to file and init functions written

--- index.js
+++ index.js
@@ -76,11 +76,21 @@ const questions = [
     }
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+        console.log('ReadMe successfully created!')
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function (data) {
+        writeToFile('ReadMe.md', generateMarkdown(data));
+    });
+};
 
 // Function call to initialize app
 init();
